cdb7329cb6673b7b662cfb6f04a2c052c8168a35
Marcelo Lima <marcelo_sp5@hotmail.com>
2019-01-08 12:56:59 -0300

    Ex 3

---
+++ JS_ES6/Exercício_2/main.js
@@ -0,0 +1,36 @@
+const usuarios = [
+    { nome: 'Diego', idade: 23, empresa: 'Rocketseat' },
+    { nome: 'Gabriel', idade: 15, empresa: 'Rocketseat' },
+    { nome: 'Lucas', idade: 30, empresa: 'Facebook' },
+   ];
+   
+
+// 2.1 Utilizando o map
+
+var idades = usuarios.map(function(item){
+    return item.idade;
+});
+
+console.log(idades);
+
+// 2.2 Utilizando o filter
+
+var specificUsuario = usuarios.filter(function(item){
+    return (item.empresa === "Rocketseat" && item.idade > 18);
+});
+
+console.log(specificUsuario);
+
+// 2.3 Utilizando o find
+
+var tabalho = usuarios.find(function(item){
+    return item.empresa === "Google";
+}); 
+
+console.log(tabalho);
+
+// 2.4 Unindo operações
+
+var idades = usuarios.map(item => ({ ...item, idade: item.idade * 2})).filter(item => item.idade <= 50);
+    
+console.log(idades);
